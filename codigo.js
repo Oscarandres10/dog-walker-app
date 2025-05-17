f3992ee4968106b69271fe52baabec0f1f9cd08a
Oscarandres10 <96912951+Oscarandres10@users.noreply.github.com>
2025-05-17 01:11:25 -0300

    Agregue el HTML y carpeta ay archivo js

---
+++ codigo.js
@@ -0,0 +1,18 @@
+// VERIFICO CONTRASEÑA MAYOR A 5, Al menos una mayuscula, una minuscula, y un numero
+function verificarContrasenia(pass) {
+  let verificacion = true;
+  let mayus = 0;
+  let minus = 0;
+  let numero = 0;
+  if (pass.length < 5) {
+    verificacion = false;
+    }
+    for (let x = 0; x < pass.length; x++) {
+        
+    
+        if () {
+
+        }
+    }
+  return verificacion;
+}
