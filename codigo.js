20fb1c11cf3b8615a2498a6190a987dd527d7ffa
Oscarandres10 <96912951+Oscarandres10@users.noreply.github.com>
2025-05-25 23:15:28 -0300

    Function de verificacion de Contraseña
    Inicie las Funciones siguientes con datos basicos:
    Funcion de Inicio Sesion
    Funcion de CerrarSession

--- codigo.js
+++ codigo.js
@@ -1,18 +1,42 @@
+// Variables Globales
+///---------------------------------///
+let login = false;
+
+//-----------------------------------//
+
 // VERIFICO CONTRASEÑA MAYOR A 5, Al menos una mayuscula, una minuscula, y un numero
 function verificarContrasenia(pass) {
-  let verificacion = true;
-  let mayus = 0;
-  let minus = 0;
-  let numero = 0;
-  if (pass.length < 5) {
-    verificacion = false;
-    }
-    for (let x = 0; x < pass.length; x++) {
-        
-    
-        if () {
+	let verificacion = true;
+	let mayus = 0;
+	let minus = 0;
+	let numero = 0;
 
-        }
-    }
-  return verificacion;
+	// Verificacion Vacio
+	if (pass.length <= 5) {
+		verificacion = false;
+	}
+
+	// Verificacion Letras
+	if (verificacion) {
+		for (let x = 0; x < pass.length; x++) {
+			let letra = pass.charAt(x);
+			let letraMinus = pass.toLowerCase().charAt(x);
+			let letraMayus = pass.toUpperCase().charAt(x);
+			if (letra === letraMayus) mayus++;
+			if (letra === letraMinus) minus++;
+			if (!isNaN(letra)) numero++;
+		}
+		// Verifico que mayus minus y numero  tengan por lo menos 1 character
+		if (minus < 1 || mayus < 1 || numero < 1) {
+			verificacion = false;
+		}
+	}
+	return verificacion;
+}
+
+function inicioSesion(user, pass) {
+	login = true;
+}
+function cerrarSession() {
+	login = false;
 }
