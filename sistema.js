f56325d90de4d2604339b5d58e4726be60f88fc7
Oscarandres10 <96912951+Oscarandres10@users.noreply.github.com>
2025-05-29 21:09:12 -0300

    Datos de sistema.js

---
+++ sistema.js
@@ -0,0 +1,13 @@
+function precargaClientes() {
+	let nom1 = "Facu";
+	let pass1 = "Abc1";
+	let perro = "Tito";
+	let tamanio = "";
+}
+
+function precargaPaseador() {
+	let nom1 = "Facu";
+	let usuario1 = "Facu";
+	let pass1 = "facu";
+	let cupo = 6;
+}
