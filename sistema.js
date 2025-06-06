8e84391ff1f8a2eadff14227a51f0e7fd5aba495
Oscarandres10 <96912951+Oscarandres10@users.noreply.github.com>
2025-06-05 23:49:09 -0300

    Validaciones Almacenar mas Funciones de Validacion

--- sistema.js
+++ sistema.js
@@ -1,15 +1,15 @@
 let clientes = new Array();
 let paseadores = new Array();
 let contrataciones = new Array();
-let logueado = null 
+let logueado = null;
 /* #### CLIENTES ####*/
 
 /* PRECARGA CLIENTES */
-precargaClientes()
+precargaClientes();
 function precargaClientes() {
 	let cliente1 = new Cliente();
 	cliente1.nombre = "JuanPerez";
-	cliente1.usuario = "JPerez"
+	cliente1.usuario = "JPerez";
 	cliente1.contrasenia = "JP123!";
 	cliente1.perro = "Rocky";
 	cliente1.tamanio = "Grande";
@@ -62,7 +62,7 @@ function precargaClientes() {
 	cliente7.perro = "Zeus";
 	cliente7.tamanio = "Grande";
 	clientes.push(cliente7);
-	
+
 	let cliente8 = new Cliente();
 	cliente8.nombre = "KarenTorres";
 	cliente8.usuario = "KTorres";
@@ -86,60 +86,51 @@ function precargaClientes() {
 	cliente10.perro = "Simba";
 	cliente10.tamanio = "Grande";
 	clientes.push(cliente10);
-
-
 }
 
-
-function precargaUnCliente(pNom, pUsuario, pPass, pPerro, pTamanio){
-	let unCliente = new Cliente ();
-		unCliente.nombre = pNom;
-		unCliente.usuario = pUsuario;
-		unCliente.contrasenia = pPass;
-		unCliente.perro = pPerro;
-		unCliente.tamanio = pTamanio;
-		clientes.push(unCliente);
-	
+function precargaUnCliente(pNom, pUsuario, pPass, pPerro, pTamanio) {
+	let unCliente = new Cliente();
+	unCliente.nombre = pNom;
+	unCliente.usuario = pUsuario;
+	unCliente.contrasenia = pPass;
+	unCliente.perro = pPerro;
+	unCliente.tamanio = pTamanio;
+	clientes.push(unCliente);
 }
 
-function login(pUsuario, pPass){
-	let valido = false
-	let i = 0
-	while (!valido && i < clientes.length){
+function login(pUsuario, pPass) {
+	let valido = false;
+	let i = 0;
+	while (!valido && i < clientes.length) {
 		let clienteX = clientes[i];
-		if(clienteX.usuario === pUsuario){
-			if(clienteX.contrasenia === pPass){
+		if (clienteX.usuario === pUsuario) {
+			if (clienteX.contrasenia === pPass) {
 				valido = true;
 				logueado = clienteX;
 			}
 		}
-		i++
+		i++;
 	}
 	let x = 0;
-	while (!valido && x < paseadores.length){
+	while (!valido && x < paseadores.length) {
 		let paseadorX = paseadores[x];
-		if(paseadorX.usuario === pUsuario){
-			
-			if(paseadorX.contrasenia === pPass){
+		if (paseadorX.usuario === pUsuario) {
+			if (paseadorX.contrasenia === pPass) {
 				valido = true;
 				logueado = paseadorX;
 			}
 		}
 		x++;
-		
-		
 	}
-	console.log(logueado)
+	console.log(logueado);
 	return valido;
 }
-
-
 
 /* #### PASEADORES ####*/
 
 /*PRECARGA PASEADORES*/
 
-precargaPaseador()
+precargaPaseador();
 function precargaPaseador() {
 	let paseador1 = new Paseador();
 	paseador1.nombre = "OscarRodriguez";
@@ -154,11 +145,10 @@ function precargaPaseador() {
 	paseador2.contrasenia = "FP123!";
 	paseador2.cupo = 6;
 	paseadores.push(paseador2);
-
 }
 
-function armarTablaPaseadores(){
-    let unaTabla = `<table border="1px">
+function armarTablaPaseadores() {
+	let unaTabla = `<table border="1px">
 	<tr>
 		<th>id</th>
 		<th>Nombre</th>
@@ -169,11 +159,10 @@ function armarTablaPaseadores(){
 		<th></th>
 
 	</tr>`;
-	for (let i = 0; i< paseadores.length; i++){
+	for (let i = 0; i < paseadores.length; i++) {
 		let unPaseador = paseadores[i];
 		//agregar validaciones. Falta agregar el "data" del boton
-		unaTabla += 
-		`<tr>
+		unaTabla += `<tr>
 			<td>${unPaseador.id}</td>
 			<td>${unPaseador.nombre}</td>
 			<td>${unPaseador.usuario}</td>
@@ -181,14 +170,11 @@ function armarTablaPaseadores(){
 			<td>${unPaseador.cupo}</td>
 			<td>${unPaseador.tipo}</td>
 			<td><input type=button value="Solicitar"></td>
-		</tr>`
-		
+		</tr>`;
 	}
-	unaTabla+= `</table>`;
-	return unaTabla
-
+	unaTabla += `</table>`;
+	return unaTabla;
 }
-
 
 /* 
 }
@@ -199,3 +185,63 @@ function mostrarTabla() {
 
 
 	*/
+
+/* ======================== */
+/* ####  VALIDACIONES  #### */
+
+function validacionRegistro(nombre, usuario, contrasenia, perro, tamanio) {
+	let valido = false;
+	if (
+		nombre.length > 0 &&
+		validarUsuario(usuario) &&
+		validarContrasenia(contrasenia) &&
+		perro.length > 0 &&
+		tamanio !== ""
+	) {
+		valido = true;
+	}
+	return valido;
+}
+
+function validarUsuario(pUsuario) {
+	let usuario = pUsuario.toLowerCase().trim(); // Convierto texto a lowercase
+	let valido = true;
+	let i = 0;
+	while (i < usuario.length && valido) {
+		let unicoUsuario = clientes[i];
+		if (unicoUsuario.usuario === usuario) {
+			valido = false;
+		}
+		i++;
+	}
+	return valido;
+}
+
+function validarContrasenia(contrasenia) {
+	let verificacion = true;
+	let mayus = 0;
+	let minus = 0;
+	let numero = 0;
+
+	// Verificacion Vacio
+	if (pass.length <= 5) {
+		verificacion = false;
+	}
+
+	// Verificacion Letras
+	if (verificacion) {
+		for (let x = 0; x < pass.length; x++) {
+			let letra = pass.charAt(x);
+			let letraMinus = pass.toLowerCase().charAt(x);
+			let letraMayus = pass.toUpperCase().charAt(x);
+			if (letra === letraMayus) mayus++;
+			if (letra === letraMinus) minus++;
+			if (!isNaN(letra)) numero++;
+		}
+		// Verifico que mayus minus y numero  tengan por lo menos 1 character
+		if (minus < 1 || mayus < 1 || numero < 1) {
+			verificacion = false;
+		}
+	}
+	return verificacion;
+}
--- ui.js
+++ ui.js
@@ -1,122 +1,107 @@
-function eventos (){
+function eventos() {
 	document.querySelector("#btnRegistarCliente").addEventListener("click", registroInterfaz); // Registrarse
-	document.querySelector("#btnLoginCliente").addEventListener("click", loginInterfaz); 
-	document.querySelector("#btnNosotros").addEventListener("click", mostrarSobreNosotros); 
-	document.querySelector("#btnOtro").addEventListener("click", otro); 
-	document.querySelector("#btnPaseadorRegistro").addEventListener("click", serUnPaseador); 
-	document.querySelector("#btnRegistrarme").addEventListener("click", almacenar); 
-	document.querySelector("#btnLogin").addEventListener("click", loginUI); 
-	document.querySelector("#bntMostrarTabla").addEventListener("click", mostrarTablaPaseadores); 
-
-
+	document.querySelector("#btnLoginCliente").addEventListener("click", loginInterfaz);
+	document.querySelector("#btnNosotros").addEventListener("click", mostrarSobreNosotros);
+	document.querySelector("#btnOtro").addEventListener("click", otro);
+	document.querySelector("#btnPaseadorRegistro").addEventListener("click", serUnPaseador);
+	document.querySelector("#btnRegistrarme").addEventListener("click", almacenar);
+	document.querySelector("#btnLogin").addEventListener("click", loginUI);
+	document.querySelector("#bntMostrarTabla").addEventListener("click", mostrarTablaPaseadores);
 }
 eventos();
 ocultarTodo();
 mostrarSobreNosotros();
 
-
 //primero oculto todo (dejando unicamente el navegador y el footer). Navegador y footer tiene seccionesNavegador y seccionesFooter como class para cuando las querramos ocultar
 function ocultarTodo() {
-    let lasSecciones = document.querySelectorAll(".secciones");
-    for (let unaSeccion of lasSecciones) {
-        unaSeccion.style.display = "none";
-    }
+	let lasSecciones = document.querySelectorAll(".secciones");
+	for (let unaSeccion of lasSecciones) {
+		unaSeccion.style.display = "none";
+	}
 }
 
-//luego muestro el sobre nostros y los botones para log in y registrarse 
+//luego muestro el sobre nostros y los botones para log in y registrarse
 
-function mostrarSobreNosotros(){
-	ocultarTodo()
+function mostrarSobreNosotros() {
+	ocultarTodo();
 	document.querySelector("#sectionSobreNosotros").style.display = "block";
 }
 
-/* #### BOTONES NAVEGADOR ####*/ 
+/* #### BOTONES NAVEGADOR ####*/
 
-function serUnPaseador(){
-	ocultarTodo()
+function serUnPaseador() {
+	ocultarTodo();
 	document.querySelector("#sectionFormularioPaseador").style.display = "block";
-
 }
-function otro(){
-	ocultarTodo()
-	//si quisieramos mostrar algo mas.... 
-
+function otro() {
+	ocultarTodo();
+	//si quisieramos mostrar algo mas....
 }
 
-
-
-/* #### REGISTRO ####*/ 
+/* #### REGISTRO ####*/
 function registroInterfaz() {
 	ocultarTodo();
 	document.querySelector("#sectionRegistrarse").style.display = "block";
 }
 
-
-/* #### LOGIN ####*/ 
+/* #### LOGIN ####*/
 
 function loginInterfaz() {
 	ocultarTodo();
 	document.querySelector("#sectionloginUsuario").style.display = "block";
 }
 
-function loginUI(){
-	let mensaje = ""
+function loginUI() {
+	let mensaje = "";
 	let usuario = document.querySelector("#txtUsuario").value;
 	let contrasenia = document.querySelector("#txtContrasenia").value;
 
-	if(login(usuario, contrasenia)){
-		ocultarTodo()
-		if(logueado.tipo === "cliente"){
-			ocultarTodo()
+	if (login(usuario, contrasenia)) {
+		ocultarTodo();
+		if (logueado.tipo === "cliente") {
+			ocultarTodo();
 			document.querySelector("#sectionUsuarioLogueado").style.display = "block";
-		
 		}
-		if(logueado.tipo === "paseador"){
-			ocultarTodo()
+		if (logueado.tipo === "paseador") {
+			ocultarTodo();
 			document.querySelector("#sectionPaseadores").style.display = "block";
-
 		}
-	}else{
+	} else {
 		mensaje = "Verifique usuario y contraseña...";
 	}
 	document.querySelector("#pLogin").innerHTML = mensaje;
-	
 }
 
 /* #### Tablas #### */
-function mostrarTablaPaseadores(){
-
+function mostrarTablaPaseadores() {
 	let laTabla = armarTablaPaseadores();
-	document.querySelector("#mostrarTablaPaseadores").innerHTML = laTabla
+	document.querySelector("#mostrarTablaPaseadores").innerHTML = laTabla;
 }
-
-
 
 /* #### ALMACENADO DE DATOS ####*/
 
-function almacenar (){
-	let mensaje = ""
+function almacenar() {
+	let mensaje = "";
 	let nombre = document.querySelector("#txtNombreCliente").value;
 	let usuario = document.querySelector("#txtUsuarioCliente").value;
 	let contrasenia = document.querySelector("#txtContraseniaCliente").value;
 	let nombrePerro = document.querySelector("#txtNombrePerroCliente").value;
 	let tamanio = document.querySelector("#selTamanioCliente").value;
-	let validaciones = true
-	//agregar validaciones 
-	if(validaciones){
+	let validaciones = validacionRegistro(nombre, usuario, contrasenia, perro, tamanio);
+	/* ####################################################### */
+	validaciones = true; // BORRAR ESTO PARA USAR VALIDACIONES
+	//agregar validaciones
+	if (validaciones) {
 		let nuevoCliente = new Cliente();
 		nuevoCliente.nombre = nombre;
 		nuevoCliente.usuario = usuario;
 		nuevoCliente.contrasenia = contrasenia;
 		nuevoCliente.perro = nombrePerro;
 		nuevoCliente.tamanio = tamanio;
-		
+
 		clientes.push(nuevoCliente);
 		mensaje = `Usuario ${nombre} registro correctamente`;
 	}
 
-	document.querySelector("#pRegistrarse").innerHTML = mensaje
+	document.querySelector("#pRegistrarse").innerHTML = mensaje;
 }
-
-
-
