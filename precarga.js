2341ba09b8cffee5974503019e9cf0ef40dc4ea4
Oscarandres10 <96912951+Oscarandres10@users.noreply.github.com>
2025-06-26 19:24:47 -0300

    Arreglo de precargas DAtos

--- precarga.js
+++ precarga.js
@@ -1,189 +1,183 @@
 // #region ## PRECARGA CLIENTES
 function precargaClientes() {
-	let nombre1 = "JuanPerez";
-	let usuario1 = "JPerez";
-	let contrasenia1 = "Jp123!";
-	let perroNombre1 = "Rocky";
-	let tamanioPerro1 = "Grande";
+	let nombre0 = "Cliente0";
+	let usuario0 = "Cliente0";
+	let contrasenia0 = "Pass0";
+	let perroNombre0 = "Rocky";
+	let tamanioPerro0 = "Grande";
+	if (miSistema.validacionRegistroCliente(nombre0, usuario0, contrasenia0, perroNombre0, tamanioPerro0)) {
+		miSistema.cargaUnCliente(nombre0, usuario0, contrasenia0, perroNombre0, tamanioPerro0);
+	}
 
+	let nombre1 = "Cliente1";
+	let usuario1 = "Cliente1";
+	let contrasenia1 = "Pass0";
+	let perroNombre1 = "Bella";
+	let tamanioPerro1 = "Mediano";
 	if (miSistema.validacionRegistroCliente(nombre1, usuario1, contrasenia1, perroNombre1, tamanioPerro1)) {
 		miSistema.cargaUnCliente(nombre1, usuario1, contrasenia1, perroNombre1, tamanioPerro1);
 	}
 
-	let nombre2 = "MariaLopez";
-	let usuario2 = "MLopez";
-	let contrasenia2 = "Ml123!";
-	let perroNombre2 = "Bella";
-	let tamanioPerro2 = "Mediano";
-
+	let nombre2 = "Cliente2";
+	let usuario2 = "Cliente2";
+	let contrasenia2 = "Pass0";
+	let perroNombre2 = "Thor";
+	let tamanioPerro2 = "Grande";
 	if (miSistema.validacionRegistroCliente(nombre2, usuario2, contrasenia2, perroNombre2, tamanioPerro2)) {
 		miSistema.cargaUnCliente(nombre2, usuario2, contrasenia2, perroNombre2, tamanioPerro2);
 	}
 
-	let nombre3 = "CarlosGomez";
-	let usuario3 = "CGomez";
-	let contrasenia3 = "Cg123!";
-	let perroNombre3 = "Thor";
-	let tamanioPerro3 = "Grande";
-
+	let nombre3 = "Cliente3";
+	let usuario3 = "Cliente3";
+	let contrasenia3 = "Pass0";
+	let perroNombre3 = "Lola";
+	let tamanioPerro3 = "Chico";
 	if (miSistema.validacionRegistroCliente(nombre3, usuario3, contrasenia3, perroNombre3, tamanioPerro3)) {
 		miSistema.cargaUnCliente(nombre3, usuario3, contrasenia3, perroNombre3, tamanioPerro3);
 	}
 
-	let nombre4 = "AnaMartinez";
-	let usuario4 = "AMartinez";
-	let contrasenia4 = "Am123!";
-	let perroNombre4 = "Lola";
-	let tamanioPerro4 = "Chico";
-
+	let nombre4 = "Cliente4";
+	let usuario4 = "Cliente4";
+	let contrasenia4 = "Pass0";
+	let perroNombre4 = "Max";
+	let tamanioPerro4 = "Mediano";
 	if (miSistema.validacionRegistroCliente(nombre4, usuario4, contrasenia4, perroNombre4, tamanioPerro4)) {
 		miSistema.cargaUnCliente(nombre4, usuario4, contrasenia4, perroNombre4, tamanioPerro4);
 	}
 
-	let nombre5 = "PedroCastro";
-	let usuario5 = "PCastro";
-	let contrasenia5 = "Pc123!";
-	let perroNombre5 = "Max";
-	let tamanioPerro5 = "Mediano";
-
+	let nombre5 = "Cliente5";
+	let usuario5 = "Cliente5";
+	let contrasenia5 = "Pass0";
+	let perroNombre5 = "Nala";
+	let tamanioPerro5 = "Chico";
 	if (miSistema.validacionRegistroCliente(nombre5, usuario5, contrasenia5, perroNombre5, tamanioPerro5)) {
 		miSistema.cargaUnCliente(nombre5, usuario5, contrasenia5, perroNombre5, tamanioPerro5);
 	}
 
-	let nombre6 = "SofiaRivera";
-	let usuario6 = "SRivera";
-	let contrasenia6 = "Sr123!";
-	let perroNombre6 = "Nala";
-	let tamanioPerro6 = "Chico";
-
+	let nombre6 = "Cliente6";
+	let usuario6 = "Cliente6";
+	let contrasenia6 = "Pass0";
+	let perroNombre6 = "Zeus";
+	let tamanioPerro6 = "Grande";
 	if (miSistema.validacionRegistroCliente(nombre6, usuario6, contrasenia6, perroNombre6, tamanioPerro6)) {
 		miSistema.cargaUnCliente(nombre6, usuario6, contrasenia6, perroNombre6, tamanioPerro6);
 	}
 
-	let nombre7 = "TomasRuiz";
-	let usuario7 = "TRuiz";
-	let contrasenia7 = "Tr123!";
-	let perroNombre7 = "Zeus";
-	let tamanioPerro7 = "Grande";
+	let nombre7 = "Cliente7";
+	let usuario7 = "Cliente7";
+	let contrasenia7 = "Pass0";
+	let perroNombre7 = "Maya";
+	let tamanioPerro7 = "Mediano";
 	if (miSistema.validacionRegistroCliente(nombre7, usuario7, contrasenia7, perroNombre7, tamanioPerro7)) {
 		miSistema.cargaUnCliente(nombre7, usuario7, contrasenia7, perroNombre7, tamanioPerro7);
 	}
 
-	let nombre8 = "KarenTorres";
-	let usuario8 = "KTorres";
-	let contrasenia8 = "Kt123!";
-	let perroNombre8 = "Maya";
-	let tamanioPerro8 = "Mediano";
+	let nombre8 = "Cliente8";
+	let usuario8 = "Cliente8";
+	let contrasenia8 = "Pass0";
+	let perroNombre8 = "Toby";
+	let tamanioPerro8 = "Chico";
 	if (miSistema.validacionRegistroCliente(nombre8, usuario8, contrasenia8, perroNombre8, tamanioPerro8)) {
 		miSistema.cargaUnCliente(nombre8, usuario8, contrasenia8, perroNombre8, tamanioPerro8);
 	}
 
-	let nombre9 = "FernandoZapata";
-	let usuario9 = "FZapata";
-	let contrasenia9 = "Fz123!";
-	let perroNombre9 = "Toby";
-	let tamanioPerro9 = "Chico";
+	let nombre9 = "Cliente9";
+	let usuario9 = "Cliente9";
+	let contrasenia9 = "Pass0";
+	let perroNombre9 = "Simba";
+	let tamanioPerro9 = "Grande";
 	if (miSistema.validacionRegistroCliente(nombre9, usuario9, contrasenia9, perroNombre9, tamanioPerro9)) {
 		miSistema.cargaUnCliente(nombre9, usuario9, contrasenia9, perroNombre9, tamanioPerro9);
 	}
 
-	let nombre10 = "LauraMendoza";
-	let usuario10 = "LMendoza";
-	let contrasenia10 = "Lm123!";
-	let perroNombre10 = "Simba";
+	let nombre10 = "Cliente10";
+	let usuario10 = "Cliente10";
+	let contrasenia10 = "Pass0";
+	let perroNombre10 = "Rex";
 	let tamanioPerro10 = "Grande";
 	if (miSistema.validacionRegistroCliente(nombre10, usuario10, contrasenia10, perroNombre10, tamanioPerro10)) {
 		miSistema.cargaUnCliente(nombre10, usuario10, contrasenia10, perroNombre10, tamanioPerro10);
 	}
 
-	let nombre11 = "LuisSanchez";
-	let usuario11 = "LSanchez";
-	let contrasenia11 = "Ls123!";
-	let perroNombre11 = "Rex";
-	let tamanioPerro11 = "Grande";
+	let nombre11 = "Cliente11";
+	let usuario11 = "Cliente11";
+	let contrasenia11 = "Pass0";
+	let perroNombre11 = "Luna";
+	let tamanioPerro11 = "Mediano";
 	if (miSistema.validacionRegistroCliente(nombre11, usuario11, contrasenia11, perroNombre11, tamanioPerro11)) {
 		miSistema.cargaUnCliente(nombre11, usuario11, contrasenia11, perroNombre11, tamanioPerro11);
 	}
 
-	let nombre12 = "CarmenVega";
-	let usuario12 = "CVega";
-	let contrasenia12 = "Cv123!";
-	let perroNombre12 = "Luna";
-	let tamanioPerro12 = "Mediano";
+	let nombre12 = "Cliente12";
+	let usuario12 = "Cliente12";
+	let contrasenia12 = "Pass0";
+	let perroNombre12 = "Buddy";
+	let tamanioPerro12 = "Grande";
 	if (miSistema.validacionRegistroCliente(nombre12, usuario12, contrasenia12, perroNombre12, tamanioPerro12)) {
 		miSistema.cargaUnCliente(nombre12, usuario12, contrasenia12, perroNombre12, tamanioPerro12);
 	}
 
-	let nombre13 = "JorgeRamirez";
-	let usuario13 = "JRamirez";
-	let contrasenia13 = "Jr123!";
-	let perroNombre13 = "Buddy";
-	let tamanioPerro13 = "Grande";
+	let nombre13 = "Cliente13";
+	let usuario13 = "Cliente13";
+	let contrasenia13 = "Pass0";
+	let perroNombre13 = "Daisy";
+	let tamanioPerro13 = "Chico";
 	if (miSistema.validacionRegistroCliente(nombre13, usuario13, contrasenia13, perroNombre13, tamanioPerro13)) {
 		miSistema.cargaUnCliente(nombre13, usuario13, contrasenia13, perroNombre13, tamanioPerro13);
 	}
 
-	let nombre14 = "PatriciaFlores";
-	let usuario14 = "PFlores";
-	let contrasenia14 = "Pf123!";
-	let perroNombre14 = "Daisy";
-	let tamanioPerro14 = "Chico";
+	let nombre14 = "Cliente14";
+	let usuario14 = "Cliente14";
+	let contrasenia14 = "Pass0";
+	let perroNombre14 = "Duke";
+	let tamanioPerro14 = "Mediano";
 	if (miSistema.validacionRegistroCliente(nombre14, usuario14, contrasenia14, perroNombre14, tamanioPerro14)) {
 		miSistema.cargaUnCliente(nombre14, usuario14, contrasenia14, perroNombre14, tamanioPerro14);
 	}
 
-	let nombre15 = "RobertoJimenez";
-	let usuario15 = "RJimenez";
-	let contrasenia15 = "Rj123!";
-	let perroNombre15 = "Duke";
-	let tamanioPerro15 = "Mediano";
+	let nombre15 = "Cliente15";
+	let usuario15 = "Cliente15";
+	let contrasenia15 = "Pass0";
+	let perroNombre15 = "Coco";
+	let tamanioPerro15 = "Chico";
 	if (miSistema.validacionRegistroCliente(nombre15, usuario15, contrasenia15, perroNombre15, tamanioPerro15)) {
 		miSistema.cargaUnCliente(nombre15, usuario15, contrasenia15, perroNombre15, tamanioPerro15);
 	}
 
-	let nombre16 = "DanielaOrtega";
-	let usuario16 = "DOrtega";
-	let contrasenia16 = "Do123!";
-	let perroNombre16 = "Coco";
-	let tamanioPerro16 = "Chico";
+	let nombre16 = "Cliente16";
+	let usuario16 = "Cliente16";
+	let contrasenia16 = "Pass0";
+	let perroNombre16 = "Bruno";
+	let tamanioPerro16 = "Grande";
 	if (miSistema.validacionRegistroCliente(nombre16, usuario16, contrasenia16, perroNombre16, tamanioPerro16)) {
 		miSistema.cargaUnCliente(nombre16, usuario16, contrasenia16, perroNombre16, tamanioPerro16);
 	}
 
-	let nombre17 = "AndresHerrera";
-	let usuario17 = "AHerrera";
-	let contrasenia17 = "Ah123!";
-	let perroNombre17 = "Bruno";
-	let tamanioPerro17 = "Grande";
+	let nombre17 = "Cliente17";
+	let usuario17 = "Cliente17";
+	let contrasenia17 = "Pass0";
+	let perroNombre17 = "Lucy";
+	let tamanioPerro17 = "Mediano";
 	if (miSistema.validacionRegistroCliente(nombre17, usuario17, contrasenia17, perroNombre17, tamanioPerro17)) {
 		miSistema.cargaUnCliente(nombre17, usuario17, contrasenia17, perroNombre17, tamanioPerro17);
 	}
 
-	let nombre18 = "MonicaSilva";
-	let usuario18 = "MSilva";
-	let contrasenia18 = "Ms123!";
-	let perroNombre18 = "Lucy";
-	let tamanioPerro18 = "Mediano";
+	let nombre18 = "Cliente18";
+	let usuario18 = "Cliente18";
+	let contrasenia18 = "Pass0";
+	let perroNombre18 = "Oreo";
+	let tamanioPerro18 = "Chico";
 	if (miSistema.validacionRegistroCliente(nombre18, usuario18, contrasenia18, perroNombre18, tamanioPerro18)) {
 		miSistema.cargaUnCliente(nombre18, usuario18, contrasenia18, perroNombre18, tamanioPerro18);
 	}
 
-	let nombre19 = "GabrielRojas";
-	let usuario19 = "GRojas";
-	let contrasenia19 = "Gr123!";
-	let perroNombre19 = "Oreo";
-	let tamanioPerro19 = "Chico";
+	let nombre19 = "Cliente19";
+	let usuario19 = "Cliente19";
+	let contrasenia19 = "Pass0";
+	let perroNombre19 = "Stella";
+	let tamanioPerro19 = "Grande";
 	if (miSistema.validacionRegistroCliente(nombre19, usuario19, contrasenia19, perroNombre19, tamanioPerro19)) {
 		miSistema.cargaUnCliente(nombre19, usuario19, contrasenia19, perroNombre19, tamanioPerro19);
-	}
-
-	let nombre20 = "BeatrizMedina";
-	let usuario20 = "BMedina";
-	let contrasenia20 = "Bm123!";
-	let perroNombre20 = "Stella";
-	let tamanioPerro20 = "Grande";
-	if (miSistema.validacionRegistroCliente(nombre20, usuario20, contrasenia20, perroNombre20, tamanioPerro20)) {
-		miSistema.cargaUnCliente(nombre20, usuario20, contrasenia20, perroNombre20, tamanioPerro20);
 	}
 }
 
@@ -192,40 +186,44 @@ function precargaClientes() {
 // #region ## PRECARGA PASEADOR
 
 function precargaPaseador() {
-	let nombre1 = "OscarRodriguez";
-	let usuario1 = "ORodriguez";
-	let contrasenia1 = "Or123!";
-	let cupo1 = 3;
+	let nombre0 = "Paseador0";
+	let usuario0 = "Paseador0";
+	let contrasenia0 = "Pass0";
+	let cupo0 = 3;
+	if (miSistema.validacionRegistroPaseador(nombre0, usuario0, contrasenia0, cupo0)) {
+		miSistema.cargaUnPaseador(nombre0, usuario0, contrasenia0, cupo0);
+	}
+
+	let nombre1 = "Paseador1";
+	let usuario1 = "Paseador1";
+	let contrasenia1 = "Pass0";
+	let cupo1 = 8;
 	if (miSistema.validacionRegistroPaseador(nombre1, usuario1, contrasenia1, cupo1)) {
 		miSistema.cargaUnPaseador(nombre1, usuario1, contrasenia1, cupo1);
 	}
-	let nombre2 = "FacundoPerdomo";
-	let usuario2 = "FPerdomo";
-	let contrasenia2 = "Fp123!";
-	let cupo2 = 8;
+
+	let nombre2 = "Paseador2";
+	let usuario2 = "Paseador2";
+	let contrasenia2 = "Pass0";
+	let cupo2 = 6;
 	if (miSistema.validacionRegistroPaseador(nombre2, usuario2, contrasenia2, cupo2)) {
 		miSistema.cargaUnPaseador(nombre2, usuario2, contrasenia2, cupo2);
 	}
-	let nombre3 = "GonzaloGentile";
-	let usuario3 = "GGentile";
-	let contrasenia3 = "Gg123!";
-	let cupo3 = 6;
+
+	let nombre3 = "Paseador3";
+	let usuario3 = "Paseador3";
+	let contrasenia3 = "Pass0";
+	let cupo3 = 12;
 	if (miSistema.validacionRegistroPaseador(nombre3, usuario3, contrasenia3, cupo3)) {
 		miSistema.cargaUnPaseador(nombre3, usuario3, contrasenia3, cupo3);
 	}
-	let nombre4 = "MatiasFarias";
-	let usuario4 = "MFarias";
-	let contrasenia4 = "Mf123!";
-	let cupo4 = 12;
+
+	let nombre4 = "Paseador4";
+	let usuario4 = "Paseador4";
+	let contrasenia4 = "Pass0";
+	let cupo4 = 10;
 	if (miSistema.validacionRegistroPaseador(nombre4, usuario4, contrasenia4, cupo4)) {
 		miSistema.cargaUnPaseador(nombre4, usuario4, contrasenia4, cupo4);
-	}
-	let nombre5 = "MarceloBielsa";
-	let usuario5 = "MBielsa";
-	let contrasenia5 = "Mb123!";
-	let cupo5 = 10;
-	if (miSistema.validacionRegistroPaseador(nombre5, usuario5, contrasenia5, cupo5)) {
-		miSistema.cargaUnPaseador(nombre5, usuario5, contrasenia5, cupo5);
 	}
 }
 
@@ -270,15 +268,6 @@ function precargaContrataciones() {
 				miSistema.cargaUnaContratacion(cliente, paseador);
 			}
 		}
-
-		/* // CARGO Y PROCESO si Cliente y Paseador Existen.
-		if (elCliente !== null && elPaseador !== null) {
-			miSistema.logueado = elPaseador; // Hago un Login para que funcione una funcionalidad
-			let idContratacion = miSistema.cargaUnaContratacion(cliente, paseador);
-			miSistema.procesarAceptarContratacion(idContratacion);
-			miSistema.validoContratacionesPendientesDespuesDeAceptar(idContratacion);
-			miSistema.logueado = null; // Hago un Logout..
-		} */
 	}
 }
 
